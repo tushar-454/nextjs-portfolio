d7c916f48d5cc4c69267ca248606dc10be1bb9ca
tushar-454 <imtushar454@gmail.com>
2024-05-12 20:03:08 +0600

    Add smooth scrolling behavior and toggle theme in settings component

---
+++ src/components/shared/Settings.tsx
@@ -0,0 +1,75 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { CiSettings } from 'react-icons/ci';
+import { FaHome } from 'react-icons/fa';
+import { MdNightlight, MdOutlineLightMode } from 'react-icons/md';
+
+const Setting = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleTheme = (theme: string) => {
+    localStorage.setItem('theme', theme);
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+      setIsDark(true);
+    } else {
+      document.documentElement.classList.remove('dark');
+      setIsDark(false);
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('theme') === 'dark') {
+      setIsDark(true);
+      document.documentElement.classList.add('dark');
+    } else if (
+      localStorage.getItem('theme') === null &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+      toggleTheme('dark');
+    } else {
+      toggleTheme('light');
+    }
+  }, []);
+
+  return (
+    <div
+      onClick={() => setIsOpen(!isOpen)}
+      className={`fixed bottom-4 right-4 cursor-pointer rounded-xl bg-gray-300 p-3`}
+    >
+      <div className='relative'>
+        <CiSettings
+          className={`animate-spin text-2xl text-gray-900 transition-all ${
+            isOpen ? 'rotate-0' : 'rotate-90'
+          } `}
+        />
+        <ul
+          className={`absolute -left-3 -top-32 flex origin-bottom flex-col gap-3 rounded-xl bg-gray-300 p-3 transition-all ${isOpen ? 'scale-y-100' : 'scale-y-0'}`}
+        >
+          <li>
+            <FaHome
+              onClick={() => window.scrollTo(0, 0)}
+              className='cursor-pointer text-2xl text-gray-900'
+            />
+          </li>
+          <li>
+            {isDark ? (
+              <MdNightlight
+                onClick={() => toggleTheme('light')}
+                className='cursor-pointer text-2xl text-gray-900'
+              />
+            ) : (
+              <MdOutlineLightMode
+                onClick={() => toggleTheme('dark')}
+                className='cursor-pointer text-2xl text-gray-900'
+              />
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Setting;
--- src/components/shared/header/Nav.tsx
+++ src/components/shared/header/Nav.tsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import { FC, useState } from 'react';
 import { MdArrowDropDown } from 'react-icons/md';
+import Settings from '../Settings';
 import SocialLinks from '../SocialLinks';
 
 interface NavProps {}
@@ -9,62 +10,65 @@ interface NavProps {}
 const Nav: FC<NavProps> = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   return (
-    <div className='flex items-center justify-center gap-10'>
-      <nav
-        className={`absolute left-0 top-[82px] w-full origin-top scale-y-0 transition-all md:static md:w-auto md:scale-y-100 ${isMenuOpen ? 'scale-y-100' : 'scale-y-0'}`}
-      >
-        <ul className='flex flex-col gap-5 bg-gray-100 p-4 md:flex-row md:gap-10 md:bg-transparent md:p-0'>
-          <li>
-            <Link
-              href='/'
-              className='font-medium text-gray-500 transition-all hover:text-black dark:text-neutral-100'
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/about'
-              className='font-medium text-gray-500 transition-all hover:text-black dark:text-neutral-100'
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/tech-stack'
-              className='font-medium text-gray-500 transition-all hover:text-black dark:text-neutral-100'
-            >
-              Tech Stack
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/projects'
-              className='font-medium text-gray-500 transition-all hover:text-black dark:text-neutral-100'
-            >
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link
-              href='/contact'
-              className='font-medium text-gray-500 transition-all hover:text-black dark:text-neutral-100'
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div className='flex items-center justify-center gap-2'>
-        <MdArrowDropDown
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-          className={`block text-5xl text-gray-400 transition-all md:hidden dark:text-neutral-100 ${isMenuOpen ? 'rotate-180' : 'rotate-0'}`}
-        />
+    <>
+      <div className='flex items-center justify-center gap-10'>
+        <nav
+          className={`absolute left-0 top-[82px] w-full origin-top scale-y-0 transition-all md:static md:w-auto md:scale-y-100 ${isMenuOpen ? 'scale-y-100' : 'scale-y-0'}`}
+        >
+          <ul className='flex flex-col gap-5 bg-gray-100 p-4 md:flex-row md:gap-10 md:bg-transparent md:p-0'>
+            <li>
+              <Link
+                href='/'
+                className='font-medium text-gray-500 transition-all hover:text-black dark:text-neutral-100'
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href='/about'
+                className='font-medium text-gray-500 transition-all hover:text-black dark:text-neutral-100'
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href='/tech-stack'
+                className='font-medium text-gray-500 transition-all hover:text-black dark:text-neutral-100'
+              >
+                Tech Stack
+              </Link>
+            </li>
+            <li>
+              <Link
+                href='/projects'
+                className='font-medium text-gray-500 transition-all hover:text-black dark:text-neutral-100'
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                href='/contact'
+                className='font-medium text-gray-500 transition-all hover:text-black dark:text-neutral-100'
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className='flex items-center justify-center gap-2'>
+          <MdArrowDropDown
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className={`block text-5xl text-gray-400 transition-all md:hidden dark:text-neutral-100 ${isMenuOpen ? 'rotate-180' : 'rotate-0'}`}
+          />
 
-        <SocialLinks />
+          <SocialLinks />
+        </div>
       </div>
-    </div>
+      <Settings />
+    </>
   );
 };
 
