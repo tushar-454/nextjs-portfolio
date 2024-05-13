64f2c7e40a3e9858b120112d2ffeb54cdf93a9d8
tushar-454 <imtushar454@gmail.com>
2024-05-13 18:28:47 +0600

    feat: Add contact page with email address

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { TbHandClick } from 'react-icons/tb';
+
+const page = () => {
+  return (
+    <main className='h-screen bg-neutral-100 dark:bg-[#191919]'>
+      <div className='flex h-full flex-col items-center gap-4 pt-40'>
+        <h1 className='text-center text-4xl font-bold text-picton-blue-900 dark:text-neutral-100'>
+          For any questions please mail us:
+        </h1>
+        <span className='relative'>
+          <Link
+            passHref={true}
+            href='mailto:imtushar454@gmail.com'
+            className='bg-gradient-to-t from-picton-pink-600 to-picton-blue-600 bg-clip-text text-center text-lg font-black text-transparent xs:text-2xl'
+          >
+            imtushar454@gmail.com
+          </Link>
+          <TbHandClick className='absolute -right-12 top-2 text-6xl text-neutral-900 dark:text-neutral-100' />
+        </span>
+      </div>
+    </main>
+  );
+};
+
+export default page;
