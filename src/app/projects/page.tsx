791ca2528753e0fb0f5772da77efce2f89a50a21
tushar-454 <imtushar454@gmail.com>
2024-05-13 18:17:33 +0600

    Add Projects page to display project cards.

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,21 @@
+import projectsDatabase, { ProjectData } from '@/Data/projects';
+import ProjectCard from '@/components/projects/ProjectCard';
+import Container from '@/components/shared/Container';
+import PageHead from '@/components/shared/PageHead';
+
+const page = () => {
+  return (
+    <main className='bg-neutral-100 dark:bg-[#191919]'>
+      <Container>
+        <PageHead title='Projects' description='Things I’ve built so far' />
+        <div className='grid grid-cols-1 gap-10 py-20 sm:grid-cols-2 lg:grid-cols-3'>
+          {projectsDatabase.map((project: ProjectData, index: number) => (
+            <ProjectCard key={index} project={project} />
+          ))}
+        </div>
+      </Container>
+    </main>
+  );
+};
+
+export default page;
