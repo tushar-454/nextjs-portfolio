56abc7ae74feacc58574aea38b73c9268c2546fb
tushar-454 <imtushar454@gmail.com>
2024-08-17 22:32:08 +0600

    refactor: Update banner image

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -8,7 +8,7 @@ const Banner = () => {
       <Container>
         <div className='flex flex-col-reverse items-center justify-between gap-5 py-20 md:flex-row'>
           <div className='w-full md:w-2/3'>
-            <h1 className='text-4xl font-bold leading-[3rem] text-east-bay-900 xss:text-5xl xss:leading-[4rem] dark:text-neutral-100'>
+            <h1 className='text-4xl font-bold leading-[3rem] text-east-bay-900 dark:text-neutral-100 xss:text-5xl xss:leading-[4rem]'>
               Hi 👋, <br /> My name is <br />{' '}
               <span className='bg-gradient-to-l from-picton-blue-500 to-picton-blue-800 bg-clip-text text-transparent'>
                 Tushar Imran
@@ -20,7 +20,7 @@ const Banner = () => {
             <Image
               src={picture}
               alt='main picture'
-              className='w-96 rounded-full border-4 border-picton-pink-500 md:w-auto dark:border-neutral-100'
+              className='w-96 rounded-full border-4 border-picton-pink-500 dark:border-neutral-100 md:w-auto'
             />
           </div>
         </div>
