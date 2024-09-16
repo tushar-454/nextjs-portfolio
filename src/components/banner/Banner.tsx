b7605d71be59288669777a5a2f3f3990fb86278b
tushar-454 <imtushar454@gmail.com>
2024-09-16 15:39:24 +0600

    refactor: Update banner content and add resume link

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -15,10 +15,19 @@ const Banner = () => {
             <h1 className='text-4xl font-bold leading-[3rem] text-east-bay-900 dark:text-neutral-100 xss:text-5xl xss:leading-[4rem]'>
               Hi 👋, <br /> My name is <br />{' '}
               <span className='bg-gradient-to-l from-picton-blue-500 to-picton-blue-800 bg-clip-text text-transparent'>
-                Tushar Imran
+                Tushar Imran.
               </span>{' '}
-              <br /> I build things for web
+              <br /> I'm a Frontend Developer.
             </h1>
+            <a
+              target='_blank'
+              href={
+                'https://drive.google.com/file/d/1AXRj-Bk4MiAL24dg4UodXrRbkaRiE_YR/view'
+              }
+              className='mt-6 inline-block whitespace-nowrap rounded-full bg-emerald-300 p-3 px-7 text-lg text-black md:mt-8'
+            >
+              View Resume
+            </a>
           </div>
           <div className='mx-auto flex justify-center md:w-1/3'>
             <Image
